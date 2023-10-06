5447ccde7c616177b7f3d232cc5e8addc0e0ce54
Olivier <olivierbilodeau@outlook.com>
2023-10-06 09:39:07 -0400

    feat: add Tab navigation + config panel screen

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,16 +1,19 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MirroirScreen from './screens/MirroirScreen';
+import ConfigPanelScreen from './screens/ConfigPanelScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={styles}>
-        <Stack.Screen name="Mirroir" component={ MirroirScreen } />
-      </Stack.Navigator>
+      <Tab.Navigator screenOptions={styles}>
+        <Tab.Screen name="Mirroir" component={ MirroirScreen } />
+        <Tab.Screen name="Config" component={ ConfigPanelScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
---
+++ src/screens/ConfigPanelScreen.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+export default function ConfigPanelScreen() {
+
+  return(
+    <Text>This is the config panel</Text>
+  );
+}
